a946ae128f80bb2721b0c54cdfbc6eef28ebb3b6
shanushikhar <thisisshanushikhar@gmail.com>
2024-06-17 19:16:24 +0530

    feat: inc/dec by value

--- begineer/src/App.tsx
+++ begineer/src/App.tsx
@@ -5,20 +5,21 @@ import { decrement, increment, byNumber } from "./redux/slices/appSlice";
 
 function App() {
   const count = useSelector((state: RootState) => state.appReducer.count);
+  const value = useSelector((state: RootState) => state.appReducer.number);
   const dispatch = useDispatch();
 
   return (
     <>
       <h1>Count value is {count}</h1>
       <div style={{ display: "flex", justifyContent: "space-around" }}>
-        <button onClick={() => dispatch(increment())}>Increase</button>
-        <button onClick={() => dispatch(decrement())}>decrease</button>
+        <button onClick={() => dispatch(increment(value))}>Increase</button>
+        <button onClick={() => dispatch(decrement(value))}>decrease</button>
       </div>
       <div style={{ margin: 10 }}>
         <input
           type="number"
           placeholder="Enter value"
-          onChange={(e) => dispatch(byNumber(e.target.value))}
+          onChange={(e) => dispatch(byNumber(+e.target.value))}
         />
       </div>
     </>
